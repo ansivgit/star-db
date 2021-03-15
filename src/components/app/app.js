994329017387153731801bd5a36af1f17d6d9194
Anna ZAS <ansiv@yandex.ru>
2021-03-15 18:23:29 +0300

    refactor: add dummy-swapy-service for testing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import {
   Header,
-  // RandomPlanet,
+  RandomPlanet,
   PersonList,
   PersonDetails,
   Row,
@@ -12,13 +12,14 @@ import {
 // import PeoplePage from '../people-page/people-page';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import SwapiService from '../../services/swapi-service';
+import DummySwapiService from '../../services/dummy-swapi-service';
 
 import './app.css';
 
 export default class App extends Component {
-  swapiService = new SwapiService();
-
   state = {
+    // swapiService: new SwapiService(),
+    swapiService: new DummySwapiService(),
     hasError: false,
   }
 
@@ -27,6 +28,16 @@ export default class App extends Component {
       hasError: true,
     });
   }
+
+  onServiceChange = () => {
+    this.setState(({ swapiService }) => {
+      const Service = swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
+
+      return {
+        swapiService: new Service(),
+      };
+    });
+  };
 
   render() {
     if (this.state.hasError) {
@@ -41,10 +52,10 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.swapiService}>
+        <SwapiServiceProvider value={this.state.swapiService}>
           <div>
-            <Header />
-            {/* <RandomPlanet /> */}
+            <Header onServiceChange={this.onServiceChange} />
+            <RandomPlanet />
 
             <Row leftItem={list} rightItem={details} />
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 import './header.css';
 
-const Header = () => {
+const Header = ({ onServiceChange }) => {
   return (
     <div className="header container d-flex">
       <h3>
@@ -22,6 +23,13 @@ const Header = () => {
           <a className="nav-link" href="#">Starships</a>
         </li>
       </ul>
+      <button
+        type="button"
+        className="btn-change-service btn btn-info"
+        onClick={onServiceChange}
+      >
+        Change service
+      </button>
     </div>
   );
 };
